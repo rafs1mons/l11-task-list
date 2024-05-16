887c8dfbae9a3f9c271c0786bbf1e328d289d5ca
Ahmet Akkaya <ahmetakkaya@192.168.1.36>
2024-05-16 23:44:05 +0300

    Thu May 16 2024 latest
    Signed-off-by: Ahmet Akkaya <ahmetakkaya@192.168.1.36>

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+Hello i'm blade template
+
+@isset($name)
+    <div>My name is {{$name}}</div>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', ['name' => 'Ahmet']);
+});
+
+Route::get('/zzz', function () {
+    return 'hello world';
+})->name('hello');
+
+Route::get('/hallo' , function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'hello ' . $name;
+});
+
+Route::fallback(function () {
+    return 'still got somewhere!';
 });
